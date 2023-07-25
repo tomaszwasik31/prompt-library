714e5a205cb1d58259996b4ee6d09e1a61549471
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-07-25 18:00:52 +0100

    initial commit, base files for project

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Prompt Library",
+  description: "Save & Share your best prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
